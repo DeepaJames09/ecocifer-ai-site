914e95b595de7af672751f918b05231233a3f81c
DeepaJames <157730081+DeepaJames09@users.noreply.github.com>
2025-06-24 09:06:09 -0400

    Update Header.tsx
    Updating with logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
           <div className="flex items-center space-x-2">
             <div className="w-8 h-8 flex items-center justify-center">
               <img 
-                src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                src="logo.png" 
                 alt="Ecocifer Logo" 
                 className="w-8 h-8 object-contain"
               />
